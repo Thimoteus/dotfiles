362a8b70ab36459cf19ddcf9bb2910acfe54b083
thimoteus <thimoteus@teknik.io>
2016-03-03 16:15:55 -0800

    much-needed update

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,10 @@
+# set theme
+set BASE16_THEME "grayscale"
+set BASE16_INTENSITY "dark"
+set BASE16_SHELL "$HOME/.config/base16-shell/base16-$BASE16_THEME.$BASE16_INTENSITY.sh"
+#eval sh $BASE16_SHELL
+eval sh "/home/thimoteus/.nvim/plugged/gruvbox/gruvbox_256palette.sh"
+
 # performs ls after cd
 function cd
   if test (count $argv) = 0
@@ -9,27 +16,46 @@ function cd
   ls
 end
 
+# check the blacklist
+function blacklist
+  cat /usr/share/doc/your-freedom/blacklist.txt | grep $argv
+end
+
 # enable vi mode
 fish_vi_mode
 
-# set theme
-set BASE16_THEME "chalk"
-set BASE16_SHELL "$HOME/.config/base16-shell/base16-$BASE16_THEME.dark.sh"
-eval sh $BASE16_SHELL
-
-# aliases
-alias edit="nvim"
-
-function vim
-  nvim
-end
-
 # worthless
-source ~/.config/fish/nvm-wrapper/nvm.fish
+#bass source ~/.nvm/nvm.sh ';' nvm use stable
 
-# start X at login
-if status --is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
-        exec startx -- -keeptty
-    end
+# promptline.vim
+function fish_prompt
+  env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.promptline.sh left
 end
+
+function fish_right_prompt
+  env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.promptline.sh right
+end
+
+# source pass stuff
+source ~/dotfiles/fish/pass.fish-completion
+
+# add bin path
+set PATH ~/bin/ $PATH
+set PATH /usr/bin/core_perl $PATH
+set PATH ~/.local/bin/ $PATH
+set PATH ~/.psvm/current/bin/ $PATH
+
+#bass source ~/dotfiles/base16-chalk.dark.sh
+
+set EDITOR nvim
+set RANGER_LOAD_DEFAULT_RC FALSE
+
+#aliases
+alias spacemacs "emacs -nw"
+#72.202.184.126
+alias vps "ssh -t -p 1005 thimoteus@unixbird.windowskrill.me tmux new-session -A -s thimotron"
+alias vps-command "ssh -p 1005 thimoteus@unixbird.windowskrill.me "
+alias pastebin "curl -F 'sprunge=<-' http://sprunge.us"
+alias irc "tmux new-session -A -s irc"
+alias zirc "ssh -t thimotron@192.168.1.5 tmux new-session -A -s irc"
+alias gotosleep "xautolock -time 60 -locker 'systemctl poweroff'"
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,53 +1,120 @@
 module Main where
 
 import XMonad
+import XMonad.ManageHook
 import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.FadeInactive
-import XMonad.Util.Run (spawnPipe)
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.UrgencyHook
+import XMonad.Util.Run
 import XMonad.Util.EZConfig (additionalKeysP)
-import XMonad.Util.Scratchpad (scratchpadSpawnActionTerminal, scratchpadManageHook)
-import XMonad.StackSet (RationalRect(..))
+import XMonad.Util.NamedScratchpad
+import XMonad.Util.Loggers (logCurrent)
+import XMonad.Hooks.EwmhDesktops
+import qualified XMonad.StackSet as W
+
+import XMonad.Util.Themes
+import XMonad.Layout.Fullscreen
+import XMonad.Layout.Tabbed
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Groups.Helpers
+import XMonad.Layout.BinarySpacePartition
+import XMonad.Layout.EqualSpacing
 
 import System.Exit
 import System.IO
 
 main = do
-  xmproc <- spawnPipe "xmobar"
-  xmonad $ conf xmproc `additionalKeysP` keyBinds
+  --dzenLeft <- spawnPipe myXMonadBar
+  --spawnPipe myStatusBar
+  xmonad . ewmh $ flip additionalKeysP keyBinds $ conf -- dzenLeft
 
-conf xmproc = defaultConfig { terminal = "urxvt"
-                            , focusFollowsMouse = False
-                            , normalBorderColor = "#000000"
-                            , focusedBorderColor = "#4a708b"
-                            , manageHook = manageDocks <+> manageHook defaultConfig <+> scManageHook
-                            , layoutHook = avoidStruts $ layoutHook defaultConfig
-                            , logHook = fadeInactiveLogHook 0.85 <+> dynamicLogWithPP xmobarPP
-                              { ppOutput = hPutStrLn xmproc
-                              , ppTitle = xmobarColor "green" "" . shorten 50 }
-                            , modMask = mod4Mask }
+--conf dzenLeft = defaultConfig { terminal = "urxvt"
+conf = defaultConfig { terminal = "urxvt"
+                     , focusFollowsMouse = False
+                     , normalBorderColor = "#000000"
+                     , focusedBorderColor = "#19c4e1"
+                     , borderWidth = 0
+                     , workspaces = myWorkspaces
+                     , manageHook = foldMap id [ manageDocks
+                       , manageHook defaultConfig
+                       , namedScratchpadManageHook scratchpads
+                       , fullscreenManageHook
+                       -- , isFullscreen --> doFullFloat
+                     ]
+                     , layoutHook = myLayout
+                     , logHook = fadeInactiveLogHook 0.6 -- >> myLogHook dzenLeft
+                     , modMask = mod4Mask }
 
-scManageHook = scratchpadManageHook $ RationalRect l t w h
-  where
-  h = 3/10
-  w = 1
-  t = 0 --1 - h
-  l = 1 - w
+currentDesktop :: X String
+currentDesktop = do
+  werk <- logCurrent
+  return $ maybe "???" id werk
 
-keyBinds = [ ("M-S-z", spawn "lock")
-           , ("M-<Return>", spawn "urxvt -cd \"`xcwd`\"")
-           , ("<Print>", spawn "scrot ~/pictures/screenshots/%Y-%m-%d-%H-%M-%S_$wx$h.png")
-           , ("M-<Space>", spawn "j4-dmenu-desktop --dmenu=\"dmenu -i -nb '#262626' -nf '#9b9b9b' -sb '#222222' -fn 'lemon'\"")
+myXMonadBar = "dzen2 -x '0' -y '0' -h '16' -w '500' -ta l"
+myStatusBar = "cave | dzen2 -p -e - -h '16' -ta r -x 400 -w 880 -y 0 -fn 'lemon:size=8'"
+
+scratchpads = [
+              NS "terminal" "urxvt --hold -name xm-terminal -e tmux new-session -A -s sp" (resource =? "xm-terminal") (customFloating $ W.RationalRect (1/4) (1/3) (1/2) (1/3))
+            -- , NS "cave" "urxvt --hold -name xm-cave -e ~/bin/cave" (resource =? "xm-cave") (customFloating $ W.RationalRect 0 (3/4) (1/4) (1/4))
+  ]
+
+myLayout = (avoidStruts . smartBorders) (equalSpacing 6 6 0 1 emptyBSP) ||| noBorders (fullscreenFull Full)
+
+myWorkspaces = [ "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X" ]
+
+myLogHook h = dynamicLogWithPP $ defaultPP
+    { ppCurrent = dzenColor "#4484de" "black" . pad
+    , ppVisible = dzenColor "#392f30" "black" . pad
+    , ppHidden = dzenColor "#392f30" "black" . pad
+    , ppHiddenNoWindows = const ""
+    , ppWsSep = ""
+    , ppTitle = take 100
+    , ppSep = " » "
+    , ppLayout = const ""
+    , ppOutput = hPutStrLn h }
+
+
+--j4dmenudesktop = "j4-dmenu-desktop --dmenu=\"dmenu -i -h 16 -nb 'black' -nf '#9b9b9b' -sb '#222222' -fn 'lemon:size=8'\""
+j4dmenudesktop = "j4-dmenu-desktop --dmenu=\"dmenu -i -q -p '>>=' -x 450 -y 400 -h 15 -w 380 -l 0 -nb '#2b303b' -nf '#8fa1b3' -sb '#8fa1b3' -sf '#2b303b' -dim 0.8 -fn 'bitocra7:size=5'\""
+dmenuStyle = init $ drop 32 j4dmenudesktop
+passmenu = "passmenu " ++ dmenuStyle
+
+keyBinds = [ ("M-<Return>", spawn "urxvt -cd \"`xcwd`\" -e fish")
+           -- , ("M-t", spawn "urxvt -cd \"`xcwd`\" -e fish")
+           , ("<Print>", spawn "scrot ~/Pictures/screenshots/%Y-%m-%d-%H-%M-%S_$wx$h.png")
+           , ("M-<Space>", spawn j4dmenudesktop)
+           , ("M-q", spawn "xmonad --recompile && xmonad --restart")
            , ("M-S-q", kill)
            , ("M-S-C-q", io $ exitWith ExitSuccess)
            , ("M-M1-<Space>", sendMessage NextLayout)
-           , ("M-b", sendMessage ToggleStruts)
-           , ("M-`", scratchpadSpawnActionTerminal "urxvt")
-           , ("<XF86ModeLock>", spawn "lock")
-           , ("<XF86AudioPlay>", spawn "playerctl play-pause")
-           , ("<XF86AudioNext>", spawn "playerctl next")
-           , ("<XF86AudioPrev>", spawn "playerctl previous") ]
-           --, ("<XF86AudioMute>", spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
-           --, ("<XF86AudioRaiseVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@ +5%")
-           --, ("<XF86AudioLowerVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@ -5%") ]
+           , ("M-n", spawn "notify-send \"`date +%T`\"")
+           , ("M-b", spawn "notify-send \"`acpi --battery`\"")
+           --, ("M-b", sendMessage ToggleStruts)
+           --, ("M-a", currentDesktop >>= \ s -> spawn ("notify-send \"Current workspace: " ++ s ++ "\""))
+           , ("M-r", spawn "~/bin/redshiftoggle")
+           , ("M-y", toggleFocusFloat)
+           , ("M-`", namedScratchpadAction scratchpads "terminal")
+           , ("M-p", spawn passmenu)
+           , ("M-[", sendMessage LessSpacing)
+           , ("M-]", sendMessage MoreSpacing)
+           , ("M-M1-h", sendMessage $ MoveSplit L)
+           , ("M-M1-l", sendMessage $ MoveSplit R)
+           , ("M-M1-k", sendMessage $ MoveSplit U)
+           , ("M-M1-j", sendMessage $ MoveSplit D)
+           , ("M-v", sendMessage Rotate)
+           , ("M-c", spawn "~/bin/comptoggle")
+           , ("<XF86Battery>", spawn "notify-send \"`acpi --battery`\"")
+           , ("<XF86ScreenSaver>", spawn "/usr/bin/lock")
+           , ("<XF86AudioPlay>", spawn "playerctl play-pause || mocp --toggle-pause")
+           , ("<XF86AudioNext>", spawn "playerctl next || mocp --next")
+           , ("<XF86AudioPrev>", spawn "playerctl previous || mocp --previous")
+           , ("<XF86AudioStop>", spawn "playerctl stop || mocp --stop")
+           , ("<XF86Sleep>", spawn "systemctl suspend")
+           , ("<XF86Launch1>", spawn "pulseaudio-ctl mute") -- "pactl set-sink-mute @DEFAULT_SINK@ toggle")
+           , ("<XF86AudioRaiseVolume>", spawn "pulseaudio-ctl up") -- "pactl set-sink-volume @DEFAULT_SINK@ +5%")
+           , ("<XF86AudioLowerVolume>", spawn "pulseaudio-ctl down") ] --"pactl set-sink-volume @DEFAULT_SINK@ -5%") ]
+
+
