9c15751336a9a2c523e2cf57d32116b9ad228eb6
thimoteus <thimoteus@teknik.io>
2016-04-13 15:40:56 -0700

    updates for chromebook

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,6 +1,10 @@
 module Main where
 
+import Data.Monoid
+import Data.List (isInfixOf)
+
 import XMonad
+import XMonad.Actions.WindowGo (raise, doF, className)
 import XMonad.ManageHook
 import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
@@ -16,12 +20,12 @@ import XMonad.Hooks.EwmhDesktops
 import qualified XMonad.StackSet as W
 
 import XMonad.Util.Themes
+import XMonad.Layout.ResizableTile
 import XMonad.Layout.Fullscreen
-import XMonad.Layout.Tabbed
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Groups.Helpers
-import XMonad.Layout.BinarySpacePartition
-import XMonad.Layout.EqualSpacing
+import XMonad.Layout.Spacing (smartSpacing)
+import XMonad.Layout.Spiral (spiral)
 
 import XMonad.Actions.CycleWS
 
@@ -29,82 +33,93 @@ import System.Exit
 import System.IO
 
 main = do
-  --dzenLeft <- spawnPipe myXMonadBar
-  --spawnPipe myStatusBar
-  xmonad . ewmh $ flip additionalKeysP keyBinds $ conf -- dzenLeft
+  bar <- spawnPipe myXMonadBar
+  xmonad . ewmh $ additionalKeysP (conf bar) keyBinds
 
---conf dzenLeft = defaultConfig { terminal = "urxvt"
-conf = defaultConfig { terminal = "urxvt"
-                     , focusFollowsMouse = False
-                     , normalBorderColor = "#000000"
-                     , focusedBorderColor = "#19c4e1"
-                     , borderWidth = 0
-                     , workspaces = myWorkspaces
-                     , manageHook = foldMap id [ manageDocks
-                       , manageHook defaultConfig
-                       , namedScratchpadManageHook scratchpads
-                       , fullscreenManageHook
-                       -- , isFullscreen --> doFullFloat
-                     ]
-                     , layoutHook = myLayout
-                     , logHook = fadeInactiveLogHook 0.6 -- >> myLogHook dzenLeft
-                     , modMask = mod4Mask }
+conf b =
+  defaultConfig { terminal = "urxvt"
+                , focusFollowsMouse = False
+                , normalBorderColor = "#000000"
+                , focusedBorderColor = "#19c4e1"
+                , borderWidth = 0
+                , handleEventHook = docksEventHook
+                , manageHook = namedScratchpadManageHook scratchpads
+                  <> fullscreenManageHook
+                  <> (appName =? "wingpanel" --> doIgnore)
+                  <> manageDocks
+                , layoutHook = myLayout
+                , startupHook = myStartupHook
+                , logHook = fadeInactiveLogHook 0.6
+                         <> dynamicLogWithPP (defaultPP { ppOutput = hPutStrLn b
+                                                        , ppCurrent = const $ lemonColor "#53FFFF" "|"
+                                                        , ppHidden = \ x -> case x of
+                                                                              "NSP" -> ""
+                                                                              _ -> "_"
+                                                        , ppHiddenNoWindows = \ x ->
+                                                          case x of
+                                                              "NSP" -> ""
+                                                              y -> lemonColor "#000000" "_"
+                                                        , ppWsSep = " "
+                                                        , ppTitle = renderTitle
+                                                        , ppLayout = const ""
+                                                        })
+                , modMask = mod4Mask }
 
-currentDesktop :: X String
-currentDesktop = do
-  werk <- logCurrent
-  return $ maybe "???" id werk
+renderTitle :: String -> String
+renderTitle = unwords . map f . words
+  where
+  f s | "/home/archurito" `isInfixOf` s = "~" <> drop 15 s
+  f s = s
 
-myXMonadBar = "dzen2 -x '0' -y '0' -h '16' -w '500' -ta l"
-myStatusBar = "cave | dzen2 -p -e - -h '16' -ta r -x 400 -w 880 -y 0 -fn 'lemon:size=8'"
+lemonColor :: String -> String -> String
+lemonColor c str = "%{F" <> c <> "}" <> str <> "%{F-}"
+
+myStartupHook = do
+  spawn $ "node ~/dotfiles/xmonad/date_script | " <> myLemonBar 50 658
+  spawn $ "node ~/dotfiles/xmonad/right_script | " <> myLemonBar 658 708
+
+myXMonadBar = myLemonBar 658 0
+
+myLemonBar :: Int -> Int -> String
+myLemonBar w x = "lemonbar -p -B '#AA000000' -o -4 -g " <> show w <> "x9+" <> show x <> " -f '-*-bitocra7-*-*-*-*-*-*-*-*-*-*-*-*' -f '-*-creep-*-*-*-*-*-*-*-*-*-*-*-*'"
 
 scratchpads = [
               NS "terminal" "urxvt --hold -name xm-terminal -e tmux new-session -A -s sp" (resource =? "xm-terminal") (customFloating $ W.RationalRect (1/4) (1/3) (1/2) (1/3))
   ]
 
-myLayout = (avoidStruts . smartBorders) (equalSpacing 6 6 0 1 emptyBSP) ||| noBorders (fullscreenFull Full)
+--myLayout = smartBorders (equalSpacing 6 6 0 1 emptyBSP) ||| noBorders (fullscreenFull Full)
+myLayout = smartBorders $ (avoidStruts . smartSpacing 2) (spiral phi) ||| noBorders (fullscreenFull Full)
+  where
+  tall = (ResizableTall 1 (3/100) (1/2) [])
+  phi = toRational $ 2 / (1 + sqrt 5 :: Double)
 
-myWorkspaces = [ "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X" ]
-
-j4dmenudesktop = "j4-dmenu-desktop --dmenu=\"dmenu -i -fn lemon -p '>>='\""
+j4dmenudesktop = "j4-dmenu-desktop --dmenu=\"dmenu -i -b -dim 0.5 -fn lemon-7 -p '>>='\""
 dmenuStyle = init $ drop 32 j4dmenudesktop
 passmenu = "passmenu " ++ dmenuStyle
 
 keyBinds = [ ("M-<Return>", spawn "urxvt -e fish")
            , ("<Print>", spawn "scrot ~/Pictures/screenshots/%Y-%m-%d-%H-%M-%S_$wx$h.png")
            , ("M-<Space>", spawn j4dmenudesktop)
-           , ("M-q", spawn "xmonad --recompile && xmonad --restart")
+           , ("M-q", spawn "xmonad --recompile && killall lemonbar && xmonad --restart")
            , ("M-S-q", kill)
            , ("M-S-C-q", io $ exitWith ExitSuccess)
            , ("M-M1-<Space>", sendMessage NextLayout)
-           , ("M-n", spawn "notify-send \"`date +%T`\"")
-           , ("M-b", spawn "notify-send \"`acpi --battery`\"")
-           , ("M-r", spawn "~/bin/redshiftoggle")
+           , ("M-n", spawn "notify-send \"time\" \"`date +%T`\"")
+           , ("M-b", spawn "notify-send \"battery status\" \"`acpi --battery`\"")
            , ("M-y", toggleFocusFloat)
            , ("C-M1-l", spawn "lock")
            , ("M-`", namedScratchpadAction scratchpads "terminal")
            , ("M-p", spawn passmenu)
-           , ("M-[", sendMessage LessSpacing)
-           , ("M-]", sendMessage MoreSpacing)
-           , ("M-M1-h", sendMessage $ MoveSplit L)
-           , ("M-M1-l", sendMessage $ MoveSplit R)
-           , ("M-M1-k", sendMessage $ MoveSplit U)
-           , ("M-M1-j", sendMessage $ MoveSplit D)
-           , ("M-v", sendMessage Rotate)
            , ("M-c", spawn "~/bin/comptoggle")
            , ("<XF86Battery>", spawn "notify-send \"`acpi --battery`\"")
            , ("<XF86ScreenSaver>", spawn "/usr/bin/lock")
-           , ("<XF86AudioPlay>", spawn "playerctl play-pause || mocp --toggle-pause")
-           , ("<XF86AudioNext>", spawn "playerctl next || mocp --next")
-           , ("<XF86AudioPrev>", spawn "playerctl previous || mocp --previous")
-           , ("<XF86AudioStop>", spawn "playerctl stop || mocp --stop")
-           , ("<XF86Sleep>", spawn "systemctl suspend")
            , ("M-<F1>", prevWS)
            , ("M-<F2>", nextWS)
            , ("M-<F6>", spawn "light -U 2")
            , ("M-<F7>", spawn "light -A 2")
-           , ("M-<F8>", spawn "pulseaudio-ctl mute")
-           , ("M-<F10>", spawn "pulseaudio-ctl up")
-           , ("M-<F9>", spawn "pulseaudio-ctl down") ]
+           , ("M-<F8>", spawn "~/bin/sound_mute")
+           , ("M-<F9>", spawn "~/bin/sound_down")
+           , ("M-<F10>", spawn "~/bin/sound_up")
+           ]
 
 
